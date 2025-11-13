9c4ac59e4c097e4c8f1148501591314787d657a2
LunaHuang <lunahuang@eslite.com>
2025-11-13 18:30:15 +0800

    init commit

---
+++ lib/jwt_revoker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jwt_revoker/version"
+
+module JwtRevoker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jwt_revoker/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module JwtRevoker
+  VERSION = "0.1.0"
+end
